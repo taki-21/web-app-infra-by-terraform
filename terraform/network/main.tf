6dcebd958ebfaeff1abbd071a725ee79cb55e0c6
taki-21 <taki.it.0208.21@gmail.com>
2022-04-11 05:59:33 +0900

    add: vpcの設定

---
+++ terraform/network/main.tf
@@ -0,0 +1,8 @@
+# VPCの設定
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.app_name
+  }
+}
---
+++ terraform/network/variables.tf
@@ -0,0 +1,4 @@
+# VPCのネットワーク範囲
+variable "vpc_cidr" {
+  default = "100.0.0.0/16"
+}
