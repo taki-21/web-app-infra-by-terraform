47633943b6668997fd576d3605b25cb4f09b1723
taki-21 <taki.it.0208.21@gmail.com>
2022-04-14 23:56:23 +0900

    add: db_instanceの追加

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -3,3 +3,24 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   description = "db subnet group of ${var.db_name}"
   subnet_ids = var.private_subnet_ids
 }
+
+resource "aws_db_instance" "db" {
+  allocated_storage = 10
+  storage_type      = "gp2"
+  engine            = var.engine
+  engine_version    = var.engine_version
+  instance_class    = var.db_instance
+
+  identifier          = var.db_name
+  username            = var.db_username
+  password            = var.db_password
+  skip_final_snapshot = true
+
+  enabled_cloudwatch_logs_exports = [
+    "error",
+    "general",
+    "slowquery"
+  ]
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+}
