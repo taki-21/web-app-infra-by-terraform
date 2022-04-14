2198982b229ded01b8b40961aa71700cd828b037
taki-21 <taki.it.0208.21@gmail.com>
2022-04-14 22:50:00 +0900

    add: dbモジュールの追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,15 @@ module "network" {
   source   = "./network"
   app_name = var.app_name
 }
+
+module "rds" {
+  source = "./rds"
+
+  app_name = var.app_name
+  db_name = var.db_name
+  db_username = var.db_username
+  db_password = var.db_password
+  vpc_id = module.network.vpc_id
+  alb_security_group = module.elb.alb_security_group
+  private_subnet_ids = module.network.private_subnet_ids
+}
