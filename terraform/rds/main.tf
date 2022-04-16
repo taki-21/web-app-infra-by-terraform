79005052af54eb6f214515a95dd8f350c1b9c02b
taki-21 <taki.it.0208.21@gmail.com>
2022-04-16 17:44:09 +0900

    add: rdsのセキュリティグループの追加

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -1,7 +1,36 @@
+resource "aws_security_group" "db_sg" {
+  name        = "${var.app_name}-mysql"
+  description = "security group on db of ${var.app_name}"
+
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.app_name}-sg of mysql"
+  }
+}
+
+resource "aws_security_group_rule" "mysql-rule" {
+  security_group_id = aws_security_group.db_sg.id
+
+  type = "ingress"
+
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = var.alb_security_group
+}
+
 resource "aws_db_subnet_group" "db_subnet_group" {
-  name = var.db_name
-  description = "db subnet group of ${var.db_name}"
-  subnet_ids = var.private_subnet_ids
+  name        = var.db_name
+  description = "db subent group of ${var.db_name}"
+  subnet_ids  = var.private_subnet_ids
 }
 
 resource "aws_db_instance" "db" {
