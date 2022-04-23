ce21962ca2027b729f6a692371e02ced6a51d92c
taki-21 <taki.it.0208.21@gmail.com>
2022-04-23 17:05:28 +0900

    add: ルーティングの設定

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -49,3 +49,10 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
+  route_table_id = aws_vpc.vpc.default_route_table_id
+}
